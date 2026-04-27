075cd97666f3662f0021bf03b4b7ea1265c3f28d
767bean69 <yxyap-wp22@student.tarc.edu.my>
2026-04-27 14:37:26 +0800

    testing

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -84,7 +84,7 @@ class HomeScreen extends StatelessWidget {
             ),
             const SizedBox(height: 4),
             const Text(
-              "Let's 12344crush today's workout!",
+              "Let's crush today's workout!",
               style: TextStyle(color: Colors.white54, fontSize: 14),
             ),
 
